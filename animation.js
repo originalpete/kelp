806a5e85615ceb29b09307a666175dba28e82373
Peter MacRobert <originalpete@gmail.com>
2020-09-15 20:02:34 -0400

    removing debug colours and smoothing out the animations

--- animation.js
+++ animation.js
@@ -1,35 +1,47 @@
+function randomSign() {
+  return Math.random() < 0.5 ? '-' : '+';
+}
+
 document.addEventListener("DOMContentLoaded", function(){
+
+  var movementBase     = 5;
+  var movementVariance = 15;
+  var durationBase     = 1000;
+  var durationVariance = 3000;
+  var delayBase        = 500;
+  var delayVariance    = 1000;
+
   anime({
     targets: 'g.horizontal rect.m.l',
-    translateX: function() {return - 10 - Math.random() * 10},
+    translateX: function() {return - movementBase - Math.random() * movementVariance},
     loop: true,
     direction: 'alternate',
     easing: 'easeInOutQuad',
-    // offset: function(){return '+=' + Math.round(Math.random() * 10000);},
-    delay: function(){return Math.random() * 500;},
-    duration: function(){return 2000 + Math.random() * 1000;}
+    // offset: function(){var o = randomSign() + '=' + Math.round(Math.random() * 10000); console.log(o); return o;},
+    delay: function(){return delayBase + Math.random() * delayVariance;},
+    duration: function(){return durationBase + Math.random() * durationVariance;}
   });
 
   anime({
     targets: 'g.vertical rect.m.t',
-    translateY: function() {return - 10 - Math.random() * 10},
+    translateY: function() {return - movementBase - Math.random() * movementVariance},
     loop: true,
     direction: 'alternate',
     easing: 'easeInOutQuad',
-    // offset: function(){return '+=' + Math.round(Math.random() * 10000);},
-    delay: function(){return Math.random() * 500;},
-    duration: function(){return 2000 + Math.random() * 1000;}
+    // offset: function(){var o = randomSign() + '=' + Math.round(Math.random() * 10000); console.log(o); return o;},
+    delay: function(){return delayBase + Math.random() * delayVariance;},
+    duration: function(){return durationBase + Math.random() * durationVariance;}
   });
 
   anime({
     targets: 'g.vertical rect.m.b',
-    translateY: function() {return + 10 + Math.random() * 10},
+    translateY: function() {return + movementBase + Math.random() * movementVariance},
     loop: true,
     direction: 'alternate',
     easing: 'easeInOutQuad',
-    // offset: function(){return '+=' + Math.round(Math.random() * 10000);},
-    delay: function(){return Math.random() * 500;},
-    duration: function(){return 2000 + Math.random() * 1000;}
+    // offset: function(){var o = randomSign() + '=' + Math.round(Math.random() * 10000); console.log(o); return o;},
+    delay: function(){return delayBase + Math.random() * delayVariance;},
+    duration: function(){return durationBase + Math.random() * durationVariance;}
   });
 
 });
